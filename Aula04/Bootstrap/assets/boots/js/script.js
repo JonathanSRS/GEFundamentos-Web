37e20a9b9007c5f4f5ea10bf3fd07af1dcdd0608
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-07-22 23:01:02 -0300

    Exercício 22/07/2021 página web usado o bootstrap

---
+++ Aula04/Bootstrap/assets/boots/js/script.js
@@ -0,0 +1,11 @@
+let nome = document.querySelector('#nome')
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+    if(email.value.indexOf('@')== -1){
+      txtEmail.innerHTML = 'E-mail inválido'
+    }else{
+        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.style.color = 'green'
+    }
+}
